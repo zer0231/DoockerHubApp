681895e3408cda881809c6c4dbc0687df65b5c84
zer0231 <pranay_zer0@protonmail.com>
2023-05-24 12:34:00 +0545

    tweaks on dashboard

--- app/src/main/java/com/zero/doockerhubapp/fragments/dashboardFragment/DashBoardFragment.kt
+++ app/src/main/java/com/zero/doockerhubapp/fragments/dashboardFragment/DashBoardFragment.kt
@@ -1,22 +1,89 @@
 package com.zero.doockerhubapp.fragments.dashboardFragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.zero.doockerhubapp.databinding.FragmentDashboardBinding
+import com.zero.doockerhubapp.fragments.dashboardFragment.viewModel.DashboardViewModel
+import com.zero.doockerhubapp.fragments.loginFragment.viewModel.LoginViewModel
+import com.zero.doockerhubapp.utils.NetworkResult
+import com.zero.doockerhubapp.utils.PersonalTokenScope
+import com.zero.doockerhubapp.utils.sharedPreferenceManager.SharedPreferenceUtil
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class DashBoardFragment : Fragment() {
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+    private val TAG = "DashBoardFragment"
+    private val dashboardViewModel: DashboardViewModel by viewModels()
+
+    @Inject
+    lateinit var userSharedPreference: SharedPreferenceUtil
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        getSummary()
+        binding.logoutBtn.setOnClickListener{
+            userSharedPreference.clearUserData()
+            findNavController().navigateUp()
+        }
+        if (userSharedPreference.getPersonalToken().equals("*")) {
+            dashboardViewModel.fetchPersonalToken("personalToken", PersonalTokenScope.ADMIN)
+            dashboardViewModel.personalTokenResponse.observe(viewLifecycleOwner) { result ->
+                when (result) {
+                    is NetworkResult.Loading -> {
+                        Log.d(TAG, "Loading")
+                        binding.test.text = "Loading"
+                    }
+
+                    is NetworkResult.Success -> {
+                        Log.d(TAG, result.data.toString())
+                        binding.test.text = result.data.toString()
+                        userSharedPreference.setPersonalToken(result.data?.get("token").toString())
+                        userSharedPreference.setUUID(result.data?.get("uuid").toString())
+                    }
+
+                    is NetworkResult.Error -> {
+                        Log.d(TAG, result.message.toString())
+
+                        binding.test.text = "${result.message} ${result.code}"
+                    }
+                }
+            }
+        }
         return binding.root
+    }
+
+    private fun getSummary() {
+
+        dashboardViewModel.fetchRepositorySummary()
+        dashboardViewModel.imageSummary.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is NetworkResult.Success -> {
+                    Log.d(TAG, result.data.toString())
+                    binding.test.text = "${result.data?.results?.get(0)?.name } \n${result.data?.results?.get(1)?.name }"
+                }
+
+                is NetworkResult.Error -> {
+                    Log.d(TAG, result.message.toString())
+                }
+
+                is NetworkResult.Loading -> {
+                    Log.d(TAG, "Loading")
+                    binding.test.text = "Loading"
+                }
+            }
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/zero/doockerhubapp/fragments/dashboardFragment/repository/DashboardRepository.kt
@@ -0,0 +1,44 @@
+package com.zero.doockerhubapp.fragments.dashboardFragment.repository
+
+import android.util.Log
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.zero.doockerhubapp.fragments.dashboardFragment.model.RepoSummary
+import com.zero.doockerhubapp.utils.APIResponse
+import com.zero.doockerhubapp.utils.NetworkResult
+import com.zero.doockerhubapp.utils.PersonalTokenScope
+import com.zero.doockerhubapp.utils.RemoteDataSource
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+@ActivityRetainedScoped
+class DashboardRepository @Inject constructor(private val remoteDataSource: RemoteDataSource) :
+    APIResponse() {
+
+    suspend fun getPersonalToken(
+        tokenLabel: String,
+        scope: String
+    ): Flow<NetworkResult<JsonObject>> {
+        val requestBody = JsonObject()
+        val jsonArray = JsonArray()
+        requestBody.addProperty("token_label", tokenLabel)
+        jsonArray.add(scope)
+        requestBody.add("scopes", jsonArray)
+        Log.d("TOKEN",requestBody.toString())
+        return flow {
+            emit(safeApiCall { remoteDataSource.postPersonalToken(requestBody) })
+        }.flowOn(Dispatchers.IO)
+    }
+
+    suspend fun getImageSummary():Flow<NetworkResult<RepoSummary>>{
+        return flow {
+            emit(safeApiCall { remoteDataSource.getImageSummary() })
+        }.flowOn(Dispatchers.IO)
+    }
+
+
+}
